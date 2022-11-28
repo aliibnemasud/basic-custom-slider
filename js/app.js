e20e960394dc1a20529660eb86bb46296f3ba359
aliibnemasud <mdmasudrony@gmail.com>
2022-11-29 02:38:00 +0600

    condition added

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const scrolls = document.querySelectorAll('.scroll');
 console.log(scrolls)
 let counter = 0;
 
-scrolls.forEach((slide, index)=> {
+scrolls.forEach((slide, index) => {
     slide.style.top = `${index * 100}%`;
 })
 
@@ -11,7 +11,7 @@ scrolls.forEach((slide, index)=> {
 const slides = document.querySelectorAll('.slide');
 console.log(slides)
 
-slides.forEach((slide, index)=> {
+slides.forEach((slide, index) => {
     slide.style.top = `${index * 100}%`;
 })
 
@@ -27,9 +27,39 @@ const goNext = () => {
 }
 
 const slideImage = () => {
-    slides.forEach((slide)=> {
+    slides.forEach((slide) => {
         slide.style.transform = `translateY(-${counter * 100}%)`;
     })
 }
+
+/// right to left slide
+
+const rLslides = document.querySelectorAll('.rl-slide');
+let rlCounter = 0;
+rLslides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`;
+})
+
+const rLslideImage = () => {
+    rLslides.forEach((slide) => {
+        slide.style.transform = `translateX(-${rlCounter * 100}%)`;
+    })
+}
+
+const rlNext = () => {
+    if (rLslides.length+1 > rlCounter) {
+        rlCounter++;
+        rLslideImage();
+    }
+}
+const rlPrev = () => {
+    if (rlCounter >= 0) {
+        rlCounter--;
+        rLslideImage();
+    }
+}
+
+
+
 
 
